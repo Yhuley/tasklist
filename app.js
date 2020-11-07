bab9a7e7400643ba337e740be0d4576cc7cd3bb9
Yurii <vasilvasilovichvasil@gmail.com>
2020-11-07 18:04:59 +0200

    remove task, delete all tasks, filter task added

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const taskInput = document.querySelector('.taskInput')
 
 // Add task
 addTask = (event) => {
-    if(taskInput.value.trim().length == 0){
+    if (taskInput.value.trim().length == 0) {
         alert("The field can`t be empty")
-    }else{
+    } else {
         // Create li
         const li = document.createElement('li')
         // Add class
@@ -35,7 +35,46 @@ addTask = (event) => {
 
     event.preventDefault()
 }
+// Remove task
+removeTask = (event) => {
+    if (event.target.className === 'deleteItem') {
+        if (confirm('Are you sure?')) {
+            event.target.parentElement.remove();
+        }
+    }
+}
+// Clear tasks
+clearTasks = (event) => {
+    if (taskList.childElementCount > 0){
+        if (confirm('Are you really want to clear your task list?')) {
+            while (taskList.firstChild) {
+                taskList.firstChild.remove()
+            }
+        }
+    }else {
+        alert('Nothing to delete')
+    }
+}
+// Filter tasks
+filterTasks = (event) => {
+    const text = event.target.value.toLowerCase()
+
+    document.querySelectorAll('.collectionItem').forEach(
+        (task) => {
+            const item = task.firstChild.textContent
+            console.log(item)
+            if(item.toLowerCase().indexOf(text) != -1){
+                task.style.display = 'block'
+            }else{
+                task.style.display = 'none'
+            }
+        }
+    )
+}
 // Load all event listeners
 (() => {
     form.addEventListener('submit', addTask)
+    taskList.addEventListener('click', removeTask)
+    removeButton.addEventListener('click', clearTasks)
+    filter.addEventListener('keyup', filterTasks)
 })()
